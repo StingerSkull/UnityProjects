a5215166fe08c3961533bf89b473bc4d3e9ba1b8
StingerSkull <p.le-borgne@outlook.fr>
2018-07-10 13:52:47 +0200

    First Commit MyFirstTest
    Player Fire a ball and move on platform

---
+++ MyFirstTest/Assets/Scripts/FireTp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTp : MonoBehaviour {
+
+    public Rigidbody2D m_Tp;                   
+    public Transform m_FireTransform;           
+
+    public float m_LaunchForce = 15f;        
+
+    private string m_FireButton;                // The input axis that is used for launching shells.
+    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+
+
+    private void Start ()
+    {
+        // The fire axis is based on the player number.
+        m_FireButton = "Fire1";
+
+    }
+
+
+    private void Update ()
+    {
+
+        if (Input.GetButtonDown(m_FireButton))
+        {
+            // ... reset the fired flag and reset the launch force.
+            m_Fired = false;
+        }
+        // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+        else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
+        {
+            // ... launch the shell.
+            Fire();
+        }
+    }
+
+
+    private void Fire ()
+    {
+        // Set the fired flag so only Fire is only called once.
+        m_Fired = true;
+
+        // Create an instance of the shell and store a reference to it's rigidbody.
+        Rigidbody2D shellInstance =
+            Instantiate (m_Tp, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody2D;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        shellInstance.velocity = m_LaunchForce * m_FireTransform.right;
+
+    }
+}
